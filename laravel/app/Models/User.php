b721ac6b786e489db763aaedd53a8e0f82ca8349
Paula Schwalm <paula.schwalm@mni.thm.de>
2025-01-20 00:33:05 +0100

    #27 - Add Ingredient Model and PantryItem. Customize user model regarding the connection

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -46,4 +47,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function pantryItems(): HasMany
+    {
+        return $this->hasMany(PantryItem::class,
+            'user_id',
+            'id');
+    }
+
 }
