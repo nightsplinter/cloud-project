09f906957c8e6bf82819520b6bdbad739cd6d707
Paula Schwalm <paula.schwalm@mni.thm.de>
2025-01-20 00:28:03 +0100

    #27 - Customize colors and sizes in the Tailwind CSS Config

--- laravel/tailwind.config.js
+++ laravel/tailwind.config.js
@@ -1,4 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
@@ -10,10 +9,40 @@ export default {
     ],
 
     theme: {
+        screens: {
+            sm: '480px',
+            md: '768px',
+            lg: '976px',
+            xl: '1440px',
+        },
+        colors: {
+            primary: '#3A621D', // Dark Green
+            secondary: '#868282', // Grey
+            background: '#F3ECE0', // Beige
+            white: '#FFFFFF', // White
+            black: '#000000', // Black
+            red: '#E3342F', // Red
+            green: '#38C172', // Green
+            blue: '#3490DC', // Blue
+            yellow: '#F6993F', // Yellow
+            purple: '#9561E2', // Purple
+            gray: '#6B7280', // Gray
+            lightgray: '#E5E7EB', // Light Gray
+          },
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                inria: ['"Inria Serif"', 'serif'],
             },
+            fontWeight: {
+                light: 300,
+                normal: 400,
+                bold: 700,
+              },
+              fontStyle: {
+                normal: 'normal',
+                italic: 'italic',
+                bold: 'bold',
+              },
         },
     },
 
