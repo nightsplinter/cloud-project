78a2f790bd5714b823a62dd2070139b62c259b84
Paula Schwalm <paula.schwalm@mni.thm.de>
2025-02-24 11:38:35 +0100

    #50 - Fix scheduler parameter

--- laravel/routes/console.php
+++ laravel/routes/console.php
@@ -1,15 +1,8 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-use App\Console\Commands\RecipeApiCommand;
 
-Artisan::command('inspire', function (): void {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
-Schedule::command(RecipeApiCommand::class, ['--count' => 5])
+Schedule::command('app:daily-api-call --count=5')
     ->daily()
-    ->at('08:00')
+    ->at('18:00')
     ->withoutOverlapping();
